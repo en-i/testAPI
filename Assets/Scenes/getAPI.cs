5f70f5bc2ba0caaa8f3bf0a135e81f08c33f3839
en-i <templebond@icloud.com>
2020-10-03 02:57:17 +0900

    true success

--- Assets/Scenes/getAPI.cs
+++ Assets/Scenes/getAPI.cs
@@ -41,12 +41,12 @@ public class getAPI : MonoBehaviour {
         } else {
             InputJson inputJson = JsonUtility.FromJson<InputJson> (www.downloadHandler.text);
             List<IMultipartFormSection> machine = new List<IMultipartFormSection> ();
-            machine.Add (new MultipartFormDataSection ("Authorization: Bearer " + inputJson.accessToken));
-            Debug.Log(inputJson.accessToken);
+            machine.Add (new MultipartFormDataSection ("text", "こんにちは"));
+            machine.Add (new MultipartFormDataSection ("source_lang", "ja"));
+            machine.Add (new MultipartFormDataSection ("target_lang", "en"));
+            Debug.Log (inputJson.accessToken);
             UnityWebRequest mw = UnityWebRequest.Post ("https://tra.mimi.fd.ai/machine_translation", machine);
-            mw.SetRequestHeader ("text", "%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF");
-            mw.SetRequestHeader ("source_lang", "ja");
-            mw.SetRequestHeader ("target_lang", "en");
+            mw.SetRequestHeader ("Authorization", "Bearer " + inputJson.accessToken);
             mw.downloadHandler = new DownloadHandlerBuffer ();
             yield return mw.SendWebRequest ();
             Debug.Log (mw.downloadHandler.text);
