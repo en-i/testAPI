0b8756d380daaf008ebbce8998c0bfba73d7f386
en-i <templebond@icloud.com>
2020-10-15 00:11:35 +0900

    water

---
+++ Assets/Scenes/LocationUpdater.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LocationUpdater : MonoBehaviour {
+    public float IntervalSeconds = 1.0f;
+    public LocationServiceStatus Status;
+    public LocationInfo Location;
+
+    public static bool getSpot;
+
+    IEnumerator Start () {
+        while (true) {
+            this.Status = Input.location.status;
+            if (Input.location.isEnabledByUser) {
+                switch (this.Status) {
+                    case LocationServiceStatus.Stopped:
+                        Input.location.Start ();
+                        break;
+                    case LocationServiceStatus.Running:
+                        this.Location = Input.location.lastData;
+                        getSpot = true;
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                // FIXME 位置情報を有効にして!! 的なダイアログの表示処理を入れると良さそう
+                Debug.Log ("location is disabled by user");
+            }
+
+            // 指定した秒数後に再度判定を走らせる
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scenes/getWaterAPI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+[Serializable]
+public class WaterJson {
+    public float Depth;
+    public string OfficeCode;
+    public string RiverCode;
+    public string SubRiverCod;
+    public int CSVScale;
+}
+public class getWaterAPI : MonoBehaviour {
+    private string longitude;
+    private string latitude;
+    public Text waterText;
+    public LocationUpdater updater;
+    private bool once;
+
+    // Start is called before the first frame update
+    void Start () {
+        once = true;
+    }
+
+    void Update () {
+        if (once) {
+            if (LocationUpdater.getSpot) {
+                StartCoroutine (getWater ());
+                once = false;
+            }
+        }
+    }
+    // Update is called once per frame
+    IEnumerator getWater () {
+        longitude = updater.Location.longitude.ToString ();
+        latitude = updater.Location.latitude.ToString ();
+        waterText.text = longitude + "\n" + latitude;
+
+        yield return null;
+        UnityWebRequest www = UnityWebRequest.Get ("https://suiboumap.gsi.go.jp/shinsuimap/Api/Public/GetMaxDepth?lon=" + longitude + "&lat=" + latitude);
+        yield return www.SendWebRequest ();
+        if (www.isNetworkError || www.isHttpError) {
+            waterText.text = www.error.ToString ();
+        } else {
+            WaterJson waterJson = JsonUtility.FromJson<WaterJson> (www.downloadHandler.text);
+            waterText.text = longitude + "\n" + latitude + "\n" + waterJson.Depth.ToString ();
+        }
+    }
+}
